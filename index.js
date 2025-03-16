c8b8e7cfcb20186524884bd674011c72c6b6184e
Gatwiri Ireri <gatwiriireri@gmail.com>
2025-03-16 18:46:58 +0300

    changed the index.js file

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-console.log('Hello World!');
-alert('Future QA Engineer!');
+console.log('Hello QA World! UPDATES 2025');
+alert('Future QA Engineer-love love this!!');
+console.log('See you around');
+console.log('See you round');
--- serious.js
+++ serious.js
@@ -1,2 +1,5 @@
 console.log('I ma loving this so much!');
 alert('QA Engineer!');
+alert('QA Engineer!');
+console.log('spent my Sunday, coding rather learning');
+console.log('they say practice makes it perfect!');

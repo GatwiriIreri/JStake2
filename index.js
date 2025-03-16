9e4d85657af3f7c974ebd6709d71a66c46e71d76
Gatwiri Ireri <gatwiriireri@gmail.com>
2025-03-16 23:42:26 +0300

    Changed content in index.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-console.log('Hello QA World! UPDATES 2025');
-alert('Future QA Engineer-love love this!!');
-console.log('See you around');
-console.log('See you round');
+//console.log('Hello QA World! UPDATES 2025');
+//alert('Future QA Engineer-love love this!!');
+// console.log('See you around');
+// console.log('See you round');
+
+let grade = 70; 
+
+if (grade > 60){
+    console.log('You are doing great!')
+} 
+else if (grade <= 70){
+    console.log('pull up your socks')
+} else {
+    console.log('We need to start remedial classes!')
+}
+
+
